e2aca6162a4c0b6579bce6d1493cc2af5f986fbe
Jamiu Ayomide Abioye <121096567+LordAyo@users.noreply.github.com>
2024-12-10 21:32:37 +0000

    the last commit

--- movie-website/src/App.tsx
+++ movie-website/src/App.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import MovieDetails from "./pages/MovieDetails";
 import Navbar from "./components/Navbar";
 import WatchList from "./pages/WatchList";
 
